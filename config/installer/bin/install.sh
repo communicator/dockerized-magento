da6c11c8fec3a0099284a9ceefbcef6367912b05
Craig Michael Thompson <craig.thompson@communicatorcorp.com>
2018-05-23 14:45:39 +0100

    fix(composer): add --ignore-platform-reqs
    Fixes issue where SOAP extension isn't enabled

--- config/installer/bin/install.sh
+++ config/installer/bin/install.sh
@@ -11,7 +11,7 @@ set -e
 #####################################
 function updateMagento() {
 	cd /var/www/html
-	composer update
+	composer update --ignore-platform-reqs
 }
 
 #####################################
